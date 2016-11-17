05e1254b208eb221959aa919030865fa56e568d1
cmohit21 <mohitchaturvedi33@gmail.com>
2016-11-17 13:31:55 +0530

    Made hell lot of automation

--- server.js
+++ server.js
@@ -5,22 +5,81 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles = {
+	'article-1':{
+title :'Article One|Mohit',
+heading:'Articl One',
+date :'Nov16,2016',
+content:'bakr'
+},
+    'article-2':{
+title :'Article Two|Mohit`',
+heading:'Articl Two',
+date :'Nov1,2016',
+content:'bakr'
+},
+    'article-3':{
+title :'Article Three|Mohit',
+heading:'Articl Three',
+date :'Nov16,2016',
+content:'bakr'
+}
+}
+
+function createTemplate (data){
+	var title =data.title;
+	var heading=data.heading;
+	var date=data.date;
+	var content=data.content;
+	
+	var html_template =`
+	<html>
+    <head>
+	    <title>
+		   ${title}
+		</title>
+		<meta name= "viewport"  content ="width=device-width, intial-scale=1" />
+		<link href="/ui/style.css" rel="stylesheet" />
+    </head>
+	
+	
+    <body>
+        <div class= "container">
+		<div>
+		    <a href="/">Home</a>
+        </div>
+        <hr/>
+		<h3>
+            ${heading}
+        </h3>
+		<div>
+		    ${date}
+		</div>
+		<div>
+		${content}
+	
+		</div>		
+      
+    </body>
+</html>
+`
+;
+
+
+return html_template
+}
+
+
 app.get('/', function (req, res) {//if /' path is requested , this function is executed
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));//we send content of this file
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));//we send content of this file
+app.get('/:articleNames', function (req, res) {
+  var articleNames=req.params.articleNames ;//provides by express framewok
+  res.send(createTemplate(articles[articleNames]));//we send content of this file
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));//we send content of this file
-});
-
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));//we send content of this file
-});
 
 
 
